271bcb6a19b588fdc1783296f1e5cdaa53fd1f1c
Rodrigo Medeiros <rodrigo.medeiros1@escolar.ifrn.edu.br>
2026-05-25 13:02:00 -0300

    Create Questao1.py

---
+++ Questao1.py
@@ -0,0 +1,8 @@
+def multiplicacao(valor1, valor2):
+    resultado = valor1 * valor2
+    return resultado
+
+num1 = float(input("Digite o primeiro valor: "))
+num2 = float(input("Digite o segundo valor: "))
+
+print("Resultado:", multiplicacao(num1, num2))
